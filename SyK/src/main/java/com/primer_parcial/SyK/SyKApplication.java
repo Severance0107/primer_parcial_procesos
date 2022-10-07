898f8d43818c01c638f625f4010fbefb55fa3d4b
Severance0107 <102986517+Severance0107@users.noreply.github.com>
2022-10-06 19:47:27 -0500

    Crud y visualizaciones

---
+++ SyK/src/main/java/com/primer_parcial/SyK/SyKApplication.java
@@ -0,0 +1,13 @@
+package com.primer_parcial.SyK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyKApplication.class, args);
+	}
+
+}
