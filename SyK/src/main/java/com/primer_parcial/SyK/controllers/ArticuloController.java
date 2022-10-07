dc288000f2c73b7882c4ca0f20c1c19db07148ea
Severance0107 <102986517+Severance0107@users.noreply.github.com>
2022-10-06 20:13:21 -0500

    Crear relacion entre articulo y categoria

--- SyK/src/main/java/com/primer_parcial/SyK/controllers/ArticuloController.java
+++ SyK/src/main/java/com/primer_parcial/SyK/controllers/ArticuloController.java
@@ -1,3 +1,99 @@
+<<<<<<< HEAD
+package com.primer_parcial.SyK.controllers;
+
+
+import com.primer_parcial.SyK.models.Articulo;
+import com.primer_parcial.SyK.repository.ArticuloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+
+public class ArticuloController {
+
+    @Autowired
+    private ArticuloRepository articuloRepository;
+
+    //----------------------------------------------Crear un articulo---------------------------------------------------
+    @PostMapping("/articulo")
+    public ResponseEntity crearArticulo(@RequestBody Articulo articulo){
+        try {
+            articuloRepository.save(articulo);
+            return new ResponseEntity(articulo, HttpStatus.CREATED);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    //--------------------------------------------Modificar un articulo-------------------------------------------------
+    @PutMapping("/articulo/{codigo}")
+    public ResponseEntity editarArticulo(@PathVariable String codigo, @RequestBody Articulo articulo ){
+        Optional<Articulo> articuloBD = articuloRepository.findByCodigo(codigo);
+        if (articuloBD.isPresent()) {
+            try {
+                articuloBD.get().setNombre(articulo.getNombre());
+                articuloBD.get().setDescripcion(articulo.getDescripcion());
+                articuloBD.get().setFechaRegistro(articulo.getFechaRegistro());
+                articuloBD.get().setCategoria(articulo.getCategoria());
+                articuloBD.get().setStock(articulo.getStock());
+                articuloBD.get().setPrecio_venta(articulo.getPrecio_venta());
+                articuloBD.get().setPrecio_compra(articulo.getPrecio_compra());
+                articuloRepository.save(articuloBD.get());
+                return new ResponseEntity(articulo, HttpStatus.OK);
+            }catch (Exception e){
+                return ResponseEntity.badRequest().build();
+            }
+        }
+        return ResponseEntity.badRequest().build();
+
+    }
+
+    //------------------------------------------Eliminar un articulo----------------------------------------------------
+
+    @DeleteMapping("articulo/{codigo}")
+    public ResponseEntity eliminarArticulo(@PathVariable String codigo){
+        Optional<Articulo> articuloBD = articuloRepository.findByCodigo(codigo);
+        if (articuloBD.isPresent()){
+            articuloRepository.delete(articuloBD.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+
+
+    //--------------------------------------------Listar Articulo por codigo--------------------------------------------
+    @GetMapping(value = "/articulo/codigo/{codigo}")
+    public ResponseEntity getArticulo(@PathVariable String codigo) {
+        Optional<Articulo> articulo = articuloRepository.findAllByCodigo(codigo);//Cambiar el Find ya que son todos los datos
+        if (articulo.isPresent()) {
+            return new ResponseEntity(articulo, HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    //----------------------------------------------Listar todos los articulos------------------------------------------
+
+    @GetMapping("/articulos")
+    public ResponseEntity listarArticulo(){
+        List<Articulo> articulo = articuloRepository.findAll();
+        if (articulo.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity(articulo, HttpStatus.OK);
+    }
+
+
+}
+
+=======
 package com.primer_parcial.SyK.controllers;
 
 
@@ -82,3 +178,4 @@ public class ArticuloController {
 
 }
 
+>>>>>>> origin/main
--- SyK/src/main/java/com/primer_parcial/SyK/repository/ArticuloRepository.java
+++ SyK/src/main/java/com/primer_parcial/SyK/repository/ArticuloRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.primer_parcial.SyK.repository;
 
 import com.primer_parcial.SyK.models.Articulo;
@@ -10,3 +11,17 @@ public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
     Optional<Articulo> findAllByCodigo(String codigo);
     Optional<Articulo> findByCodigo(String codigo);
 }
+=======
+package com.primer_parcial.SyK.repository;
+
+import com.primer_parcial.SyK.models.Articulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
+    Optional<Articulo> findAllByCodigo(String codigo);
+    Optional<Articulo> findByCodigo(String codigo);
+}
+>>>>>>> origin/main
