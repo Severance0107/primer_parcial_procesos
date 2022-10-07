daf4d47a2cc8d3f80a5df5d68aa35425558e9958
Severance0107 <102986517+Severance0107@users.noreply.github.com>
2022-10-06 20:47:51 -0500

    Parcial completo, verificando que la categoria

--- SyK/src/main/java/com/primer_parcial/SyK/models/Articulo.java
+++ SyK/src/main/java/com/primer_parcial/SyK/models/Articulo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.primer_parcial.SyK.models;
 
 
@@ -36,42 +35,3 @@ public class Articulo {
     private Float precio_venta;
 
 }
-=======
-package com.primer_parcial.SyK.models;
-
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.Date;
-
-
-@Data
-@NoArgsConstructor
-@Entity
-@Table(name = "almacen")
-
-public class Articulo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(length = 100, nullable = false)
-    private String nombre;
-    @Column(length = 50, nullable = false)
-    private String codigo;
-    @Column(length = 300, nullable = false)
-    private String descripcion;
-    @Column(length = 100, nullable = false)
-    private Date fechaRegistro;
-    @Column(length = 300)
-    private String categoria;
-    @Column(length = 100)
-    private Integer stock;
-    @Column(length=400, nullable= false)
-    private Float precio_compra;
-    @Column(length=400, nullable= false)
-    private Float precio_venta;
-
-}
->>>>>>> origin/main
--- SyK/src/main/java/com/primer_parcial/SyK/repository/ArticuloRepository.java
+++ SyK/src/main/java/com/primer_parcial/SyK/repository/ArticuloRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.primer_parcial.SyK.repository;
 
 import com.primer_parcial.SyK.models.Articulo;
@@ -11,17 +10,3 @@ public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
     Optional<Articulo> findAllByCodigo(String codigo);
     Optional<Articulo> findByCodigo(String codigo);
 }
-=======
-package com.primer_parcial.SyK.repository;
-
-import com.primer_parcial.SyK.models.Articulo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
-    Optional<Articulo> findAllByCodigo(String codigo);
-    Optional<Articulo> findByCodigo(String codigo);
-}
->>>>>>> origin/main
