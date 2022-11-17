e883491128ff220a892ae83d5f8101e9f37fecc5
Sergio Ortiz <saortizi@ufpso.edu.co>
2022-11-16 21:24:54 -0500

    Creacion del Usuario controller

--- SyK/src/main/java/com/primer_parcial/SyK/repository/UsuarioRepository.java
+++ SyK/src/main/java/com/primer_parcial/SyK/repository/UsuarioRepository.java
@@ -4,4 +4,8 @@ import com.primer_parcial.SyK.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByCorreo(String correo);
+
 }
+
