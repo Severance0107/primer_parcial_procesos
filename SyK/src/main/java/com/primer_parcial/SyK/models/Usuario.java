82e4368704423e6e83d73340aafc0f9f51c7f06c
Sergio Ortiz <saortizi@ufpso.edu.co>
2022-11-16 20:42:46 -0500

    Model de Usuario

---
+++ SyK/src/main/java/com/primer_parcial/SyK/models/Usuario.java
@@ -0,0 +1,20 @@
+package com.primer_parcial.SyK.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @Column(length = 45,nullable= false)
+    private String user;
+    @Column(length = 45,nullable = false)
+    private String password;
+
+
+}
--- SyK/src/main/java/com/primer_parcial/SyK/repository/UsuarioRepository.java
+++ SyK/src/main/java/com/primer_parcial/SyK/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.primer_parcial.SyK.repository;
 
+import com.primer_parcial.SyK.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
